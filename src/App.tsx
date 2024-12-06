669a3fff1971f2dc31ef05d1634350e1e827286f
arieruov <arielornelas.v@gmail.com>
2024-12-05 23:20:53 -0600

    Applied Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,67 @@
 import { useReducer, useState } from "react";
+import { GrFormAdd } from "react-icons/gr";
 import "./App.css";
 
 const reducer = (
-	state: string[],
-	action: { type: string; newTask: string }
+  state: string[],
+  action: { type: string; newTask: string }
 ) => {
-	switch (action.type) {
-		case "add":
-			return [...state, action.newTask];
+  switch (action.type) {
+    case "add":
+      return [...state, action.newTask];
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 const initialState: string[] = [];
 
 const App = () => {
-	const [state, dispatch] = useReducer(reducer, initialState);
-	const [task, setTask] = useState("");
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [task, setTask] = useState("");
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-		if (task.length > 0) {
-			dispatch({ type: "add", newTask: task });
-			setTask("");
-		} else {
-			alert("You can't add an empty task");
-		}
-	};
+    if (task.length > 0) {
+      dispatch({ type: "add", newTask: task });
+      setTask("");
+    } else {
+      alert("You can't add an empty task");
+    }
+  };
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setTask(event.target.value);
-	};
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
 
-	const taskItems = state.map((task, index) => {
-		return <li key={index}>{task}</li>;
-	});
+  const taskItems = state.map((task, index) => {
+    return (
+      <li className="Task-Item" key={index}>
+        {task}
+      </li>
+    );
+  });
 
-	return (
-		<div>
-			<h1>Todo List</h1>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="Task"
-					value={task}
-					onChange={handleChange}
-				/>
-				<button type="submit">Add</button>
-			</form>
-			{state.length > 0 && <ul>{taskItems}</ul>}
-		</div>
-	);
+  return (
+    <div className="Todo-Container">
+      <h1 className="App-Title">To-do List</h1>
+      <form className="NewTask-Form" onSubmit={handleSubmit}>
+        <input
+          className="NewTask-Input"
+          type="text"
+          placeholder="Task"
+          value={task}
+          onChange={handleChange}
+        />
+        <button className="NewTask-Submit" type="submit">
+          <GrFormAdd color="white" fontSize="1.5rem" />
+        </button>
+      </form>
+      {state.length > 0 && <ul className="Task-List">{taskItems}</ul>}
+    </div>
+  );
 };
 
 export default App;
